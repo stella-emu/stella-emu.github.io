d69ed2c893a546eb2fd813166ec8a61fcd09a990
stephena <stephena@8b62c5a3-ac7e-4cc8-8f21-d9a121418aba>
2010-11-12 19:17:34 +0000

    Updated webpage for 3.3 release.

--- footer.php
+++ footer.php
@@ -2,4 +2,4 @@
 <a href="http://www.bradfordmott.com">Bradford W. Mott</a>, 
 <a href="http://minbar.org">Stephen Anthony</a> and 
 <a href="theteam.php">The Stella Team</a>
-<br />	Site Updated September 17, 2010</p>
+<br />	Site Updated November 12, 2010</p>
