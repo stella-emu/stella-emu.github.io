267dce86423244492683147976d0b664d11f6eb9
stephena <stephena@8b62c5a3-ac7e-4cc8-8f21-d9a121418aba>
2010-11-20 17:41:39 +0000

    Added EXE installation for Windows 98/2000 release.

--- htdocs/footer.php
+++ htdocs/footer.php
@@ -2,4 +2,4 @@
 <a href="http://www.bradfordmott.com">Bradford W. Mott</a>, 
 <a href="http://minbar.org">Stephen Anthony</a> and 
 <a href="theteam.php">The Stella Team</a>
-<br />	Site Updated November 12, 2010</p>
+<br />	Site Updated November 20, 2010</p>
