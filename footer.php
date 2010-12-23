e9626de54f8dcf7921a5a7feb52f022aa340aaa3
stephena <stephena@8b62c5a3-ac7e-4cc8-8f21-d9a121418aba>
2010-12-23 21:25:00 +0000

    Oops, forgot to include the modification date.

--- footer.php
+++ footer.php
@@ -2,4 +2,4 @@
 <a href="http://www.bradfordmott.com">Bradford W. Mott</a>, 
 <a href="http://minbar.org">Stephen Anthony</a> and 
 <a href="theteam.php">The Stella Team</a>
-<br />	Site Updated November 20, 2010</p>
+<br />	Site Updated December 23, 2010</p>
