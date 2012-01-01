b2e83bc98a944d44aa7ef38b00ed196d1cb5b34e
stephena <stephena@8b62c5a3-ac7e-4cc8-8f21-d9a121418aba>
2012-01-01 02:09:38 +0000

    Updates Donations page and Credits list.

--- htdocs/footer.php
+++ htdocs/footer.php
@@ -2,4 +2,4 @@
 <a href="http://www.bradfordmott.com">Bradford W. Mott</a>, 
 <a href="http://minbar.org">Stephen Anthony</a> and 
 <a href="theteam.php">The Stella Team</a>
-<br />	Site Updated December 30, 2011</p>
+<br />	Site Updated December 31, 2011</p>
