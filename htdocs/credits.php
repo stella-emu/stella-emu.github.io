9b5bbb34be4c3e1881165e4b9732dab2610cb7c7
stephena <stephena@8b62c5a3-ac7e-4cc8-8f21-d9a121418aba>
2012-05-24 18:02:40 +0000

    More webpage updates.

--- htdocs/credits.php
+++ htdocs/credits.php
@@ -40,34 +40,34 @@
     </tr>
 
     <tr>
-      <th>8-Bit Classics<br>(<a href="http://www.8bitclassics.com">http://www.8bitclassics.com</a>)</td>
+      <th>8-Bit Classics<br>(<a href="http://www.8bitclassics.com">http://www.8bitclassics.com</a>)</th>
       <td>Offered a very good price on a Commodore 128D</td>
     </tr>
 
     <tr>
-      <th>Ant<br>(<a href="http://aqfl.net">http://aqfl.net</a>)</td>
+      <th>Ant<br>(<a href="http://aqfl.net">http://aqfl.net</a>)</th>
       <td>Provided much feedback and testing which eventually led to a fix
       for the infamous ATI OpenGL 'red screen' issue</td>
     </tr>
 
     <tr>
-      <th>David&nbsp;Aspell</td>
+      <th>David&nbsp;Aspell</th>
       <td>Ported release 0.7 of Stella to Java</td>
     </tr>
 
     <tr>
-      <th>Christopher&nbsp;Bennett</td>
+      <th>Christopher&nbsp;Bennett</th>
       <td>Helped discover a Superchanger emulation bug in release 1.0 of Stella</td>
     </tr>
 
     <tr>
-      <th>Alexander&nbsp;Bilstein</td>
+      <th>Alexander&nbsp;Bilstein</th>
       <td>Provided "A Brief History of the Atari 2600" for this manual</td>
     </tr>
 
     <tr>
       <th>Ian&nbsp;Bogost and the Georgia&nbsp;Tech&nbsp;Atari&nbsp;Team<br>
-      (<a href="http://www.bogost.com/games/a_television_simulator.shtml">CRT Simulation Project</a>)</td>
+      (<a href="http://www.bogost.com/games/a_television_simulator.shtml">CRT Simulation Project</a>)</th>
       <td>Contributed code for the 'CRT simulation' OpenGL filters.  The Winter 2009
       team members are Edward Booth, Michael Cook, Justin Dobbs, William Rowland
       and Prince Yang.<br>Also contributed <a href="http://slack.net/~ant/libs/ntsc.html">Blargg filtering</a>
@@ -76,274 +76,276 @@
     </tr>
 
     <tr>
-      <th>Dan&nbsp;Boris</td>
+      <th>Dan&nbsp;Boris</th>
       <td>Provided technical information on the Supercharger</td>
     </tr>
 
     <tr>
       <th>"Uncle"&nbsp;Carmine<br>
-      (<a href="http://www.carmine.com">http://www.carmine.com</a>)</td>
+      (<a href="http://www.carmine.com">http://www.carmine.com</a>)</th>
       <td>Generously donated several 2600 games and manuals to help with development</td>
     </tr>
 
     <tr>
-      <th>Piero&nbsp;Cavina</td>
+      <th>Piero&nbsp;Cavina</th>
       <td>Allowed "Oystron" to be included in the Stella distribution</td>
     </tr>
 
     <tr>
-      <th>Bob&nbsp;Colbert</td>
+      <th>Bob&nbsp;Colbert</th>
       <td>Allowed "Okie Dokie" to be included in the Stella distribution</td>
     </tr>
 
     <tr>
-      <th>Joe&nbsp;D'Andrea</td>
+      <th>Joe&nbsp;D'Andrea</th>
       <td>Author of the IRIX port of Stella until release 1.2</td>
     </tr>
 
     <tr>
-      <th>Renato&nbsp;Ferreira</td>
+      <th>Renato&nbsp;Ferreira</th>
       <td>Provided code to support private colormaps under Unix</td>
     </tr>
 
     <tr>
-      <th>Ron&nbsp;Fries</td>
+      <th>Ron&nbsp;Fries</th>
       <td>Author of the awesome TIA Sound library</td>
     </tr>
 
     <tr>
-      <th>Aaron&nbsp;Giles</td>
+      <th>Aaron&nbsp;Giles</th>
       <td>Author of the Power Macintosh version of Stella until release 1.1</td>
     </tr>
 
     <tr>
-      <th>Goldenegg (on AtariAge)</td>
+      <th>Goldenegg (on AtariAge)</th>
       <td>Generously donated a monetary contribution, and provided feedback/testing
       for OSX versions</td>
     </tr>
 
     <tr>
       <th>Mark&nbsp;Grebe<br>
-      (<a href="http://www.atarimac.com">http://www.atarimac.com</a>)</td>
+      (<a href="http://www.atarimac.com">http://www.atarimac.com</a>)</th>
       <td>Author of the Macintosh OSX version of Stella from releases
       1.4 to 2.4; helped with Stelladaptor support</td>
     </tr>
 
     <tr>
       <th>Sean&nbsp;Green (Bliss-Box)<br>
-      (<a href="http://spawnlinux.dyndns.org/Bliss-Box">http://spawnlinux.dyndns.org/Bliss-Box/</a>)</td>
+      (<a href="http://spawnlinux.dyndns.org/Bliss-Box">http://spawnlinux.dyndns.org/Bliss-Box/</a>)</th>
       <td>Generously donated a Bliss-Box adaptor to help with development</td>
     </tr>
 
     <tr>
       <th>Tom&nbsp;Hafner<br>
-      (<a href="http://2600-daptor.com">http://2600-daptor.com</a>)</td>
+      (<a href="http://2600-daptor.com">http://2600-daptor.com</a>)</th>
       <td>Generously donated a 2600-daptor, 2600-daptor II and BoosterGrip controllers,
       as well as sample code to help with development</td>
     </tr>
 
     <tr>
-      <th>Mark&nbsp;Hahn</td>
+      <th>Mark&nbsp;Hahn</th>
       <td>Allowed "Elk Attack" to be included in the Stella distribution and
       provided help with the TIA HMOVE blank bug</td>
     </tr>
 
     <tr>
-      <th>Alex&nbsp;Herbert</td>
+      <th>Alex&nbsp;Herbert</th>
       <td>Provided much information on AtariVox and SaveKey functionality</td>
     </tr>
 
     <tr>
-      <th>Kevin&nbsp;Horton</td>
+      <th>Kevin&nbsp;Horton</th>
       <td>Author of the definitive Atari 2600 bank-switching document</td>
     </tr>
 
     <tr>
-      <th>Richard&nbsp;Hutchinson</td>
+      <th>Richard&nbsp;Hutchinson</th>
       <td>Generously donated an AtariVox USB adaptor to help with AVox development</td>
     </tr>
 
     <tr>
-      <th>Thomas&nbsp;Jentzsch</td>
+      <th>Thomas&nbsp;Jentzsch</th>
       <td>Provided updated NTSC palette and many ideas</td>
     </tr>
 
     <tr>
-      <th>Andrew&nbsp;Kator<br>(<a href="http://www.katorlegaz.com">http://www.katorlegaz.com</a>)</td>
+      <th>Andrew&nbsp;Kator<br>(<a href="http://www.katorlegaz.com">http://www.katorlegaz.com</a>)</th>
       <td>Provided PHP and CSS code for a major update to the Stella website</td>
     </tr>
 
     <tr>
-      <th>Richard&nbsp;Kennehan</td>
+      <th>Richard&nbsp;Kennehan</th>
       <td>Generously donated Atari light-sixer, various controllers, and several games to help
         in testing ROMs on a real system</td>
     </tr>
 
     <tr>
-      <th>Sandro de Oliveira Maia</td>
+      <th>Sandro de Oliveira Maia</th>
       <td>Generously donated several monetary contributions, and provided words of support</td>
     </tr>
 
     <tr>
       <th>Nick&nbsp;Montfort and Ian&nbsp;Bogost<br>
-      (<a href="http://mitpress.mit.edu/catalog/item/default.asp?ttype=2&tid=11696">Racing the Beam</a>)</td>
+      (<a href="http://mitpress.mit.edu/catalog/item/default.asp?ttype=2&tid=11696">Racing the Beam</a>)</th>
       <td>Specifically mentioned Stella in their book</td>
     </tr>
 
     <tr>
-      <th>Kostas&nbsp;Nakos</td>
+      <th>Kostas&nbsp;Nakos</th>
       <td>Author/maintainer of the WinCE version of Stella from releases 2.0
       to  2.3.5</td>
     </tr>
 
     <tr>
-      <th>Erik&nbsp;Kovach</td>
+      <th>Erik&nbsp;Kovach</th>
       <td>Author of the property file for release 0.7, 1.0, 1.1, and 1.2 of Stella</td>
     </tr>
 
     <tr>
-      <th>Daniel&nbsp;Marks</td>
+      <th>Daniel&nbsp;Marks</th>
       <td>Provided improved keyboard joystick support for Stella release 0.1</td>
     </tr>
 
     <tr>
-      <th>James&nbsp;Mcclain</td>
+      <th>James&nbsp;Mcclain</th>
       <td>Provided patches for the 1.2 release to get Stella to compile with GCC 3.0</td>
     </tr>
 
     <tr>
-      <th>David&nbsp;McEwen</td>
+      <th>David&nbsp;McEwen</th>
       <td>Author of the Acorn version of Stella</td>
     </tr>
 
     <tr>
-      <th>Jeff&nbsp;Miller</td>
+      <th>Jeff&nbsp;Miller</th>
       <td>Author of the Windows version of Stella until release 1.2</td>
     </tr>
 
     <tr>
-      <th>Dan&nbsp;Mowczan</td>
+      <th>Dan&nbsp;Mowczan</th>
       <td>Provided a Supercharger to help with Stella's development</td>
     </tr>
 
     <tr>
-      <th>Jack&nbsp;Nutting</td>
+      <th>Jack&nbsp;Nutting</th>
       <td>Author of the OpenStep version of Stella</td>
     </tr>
 
     <tr>
-      <th>Omegamatrix</td>
+      <th>Omegamatrix</th>
       <td>Provided test ROMs which led to more accurate emulation for SWCHx/SWxCNT
       and RIOT behaviour, and for 'illegal' TIA reads</td>
     </tr>
 
     <tr>
-      <th>John&nbsp;Payson</td>
+      <th>John&nbsp;Payson</th>
       <td>Provided EEPROM emulation code for AtariVox and SaveKey support</td>
     </tr>
 
     <tr>
-      <th>Wilbert&nbsp;Pol</td>
+      <th>Wilbert&nbsp;Pol</th>
       <td>Provided many ideas and some sample algorithms for greatly improving
       'illegal HMOVE' emulation</td>
     </tr>
 
     <tr>
-      <th>Manuel&nbsp;Polik</td>
+      <th>Manuel&nbsp;Polik</th>
       <td>Author of the Windows version of Stella (Cyberstella) starting with the 1.2
       release until its retirement</td>
     </tr>
 
     <tr>
-      <th>Jim&nbsp;Pragit</td>
+      <th>Jim&nbsp;Pragit</th>
       <td>Author of the "Game Menu" emulator game shell</td>
     </tr>
 
     <tr>
-      <th>James&nbsp;Randall (aka&nbsp;'toymailman')</td>
+      <th>James&nbsp;Randall (aka&nbsp;'toymailman')</th>
       <td>Generously donated a 7800 console and several games and manuals to help
       with development</td>
     </tr>
 
     <tr>
-      <th>Fred X. Quimby (aka 'batari')<br>(<a href="http://bataribasic.com/">Batari Basic</a>)</td>
+      <th>Fred X. Quimby (aka 'batari')<br>(<a href="http://bataribasic.com/">Batari Basic</a>)</th>
       <td>Generously donated several Melody and Harmony Cartridges to help with
       development, and also donated a collectors edition of the Harmony Cart (release
       version including the cart, CD, SD card, etc)</td>
     </tr>
 
     <tr>
-      <th>RomHunter<br>(<a href="http://www.atarimania.com/rom_collection_archive_atari_2600_roms.html">RomHunter ROMs</a>)</td>
+      <th>RomHunter<br>(<a href="http://www.atarimania.com/rom_collection_archive_atari_2600_roms.html">RomHunter ROMs</a>)</th>
       <td>Provides an updated database of ROM information on a regular basis</td>
     </tr>
 
     <tr>
-      <th>John&nbsp;Saeger</td>
+      <th>John&nbsp;Saeger</th>
       <td>Participated in a helpful discussion on understanding the TIA's
       RESPx multi-sprite trick</td>
     </tr>
 
     <tr>
-      <th>Chris&nbsp;Salomon</td>
+      <th>Chris&nbsp;Salomon</th>
       <td>Provided information and code to help implement Supercharger support</td>
     </tr>
 
     <tr>
-      <th>Glenn&nbsp;Saunders</td>
+      <th>Glenn&nbsp;Saunders</th>
       <td>Generously donated a Supercharger to help with Stella's development</td>
     </tr>
 
     <tr>
-      <th>Jason&nbsp;Scott</td>
+      <th>Jason&nbsp;Scott</th>
       <td>Organizer of the property file archive for early versions of
       Stella</td>
     </tr>
 
     <tr>
-      <th>Seagtgruff</td>
+      <th>Seagtgruff</th>
       <td>Provided test ROMs which finally fixed all remaining issues with
       ADC/SBC opcode decimal mode emulation</td>
     </tr>
 
     <tr>
-      <th>David&nbsp;Shaw</td>
+      <th>David&nbsp;Shaw</th>
       <td>Provided make targets for the BSDI platform</td>
     </tr>
 
     <tr>
-      <th>Raul&nbsp;Silva</td>
+      <th>Raul&nbsp;Silva</th>
       <td>Helped with design and graphics for an early version of the Stella
       web site</td>
     </tr>
 
     <tr>
-      <th>James Smith</td>
+      <th>James Smith</th>
       <td>Generously donated a monetary contribution</td>
     </tr>
 
     <tr>
-      <th>Chris&nbsp;Snell</td>
+      <th>Chris&nbsp;Snell</th>
       <td>Maintained a mirror of the Stella FTP site</td>
     </tr>
 
     <tr>
-      <th>Spacedueler (on AtariAge)</td>
+      <th>Spacedueler (on AtariAge)</th>
       <td>Generously donated several homemade 'spinners' to help with testing
       the mouse "specific axis" mode</td>
     </tr>
 
     <tr>
-      <th>Darrell&nbsp;Spice&nbsp;Jr.</td>
-      <td>Author of the OS/2 version of Stella</td>
+      <th>Darrell&nbsp;Spice&nbsp;Jr.<br>
+      (<a href="http://spiceware.org">http://spiceware.org</a>)</th>
+      <td>Author of the OS/2 version of Stella<br>Also generously donated 'Medieval Mayhem'
+      and 'Stellas Stocking' cartridges</td>
     </tr>
 
     <tr>
-      <th>John&nbsp;Stiles</td>
+      <th>John&nbsp;Stiles</th>
       <td>Maintained the Macintosh version of Stella until release 1.2</td>
     </tr>
 
     <tr>
-      <th>Eckhard&nbsp;Stolberg</td>
+      <th>Eckhard&nbsp;Stolberg</th>
       <td>Provided a description of the TIA bug that produces the star field
       effect in Cosmic Ark, provided the PAL television palette, and
       participated in a helpful discussion on understanding the TIA's RESPx
@@ -352,41 +354,41 @@
     </tr>
 
     <tr>
-      <th>Matthew&nbsp;Stroup</td>
+      <th>Matthew&nbsp;Stroup</th>
       <td>Author of the Amiga version of Stella</td>
     </tr>
 
     <tr>
-      <th>Joel&nbsp;Sutton</td>
+      <th>Joel&nbsp;Sutton</th>
       <td>Author of the FreeBSD version of Stella</td>
     </tr>
 
     <tr>
-      <th>Greg&nbsp;Troutman</td>
+      <th>Greg&nbsp;Troutman</th>
       <td>Allowed "This Planet Sucks" to be included in the Stella distribution</td>
     </tr>
 
     <tr>
-      <th>Robert&nbsp;Tuccitto</td>
+      <th>Robert&nbsp;Tuccitto</th>
       <td>Provided valuable bugtesting and feedback which finally helped in fixing
       a long-standing lockup which occurred after Stella has been running for
       71 minutes.</td>
     </tr>
 
     <tr>
-      <th>Curt&nbsp;Vendel</td>
+      <th>Curt&nbsp;Vendel</th>
       <td>Provided schematics of the TIA which are being used to improve the
       TIA emulation.</td>
     </tr>
 
     <tr>
-      <th>David&nbsp;Voswinkel</td>
+      <th>David&nbsp;Voswinkel</th>
       <td>Maintainer of the PSP version of Stella starting with the 2.0 release
       until release 2.2</td>
     </tr>
 
     <tr>
-      <th>Brian&nbsp;Watson</td>
+      <th>Brian&nbsp;Watson</th>
       <td>Helped with getting the illegal CPU instruction support working with Stella.
       Brian also submitted a number of other changes, such as debugger support, which
       have been finally integrated into the 2.0 release of Stella.  Without
@@ -395,24 +397,24 @@
     </tr>
 
     <tr>
-      <th>Keith&nbsp;Wilkins</td>
+      <th>Keith&nbsp;Wilkins</th>
       <td>Maintained the DOS version of Stella until release 0.7</td>
     </tr>
 
     <tr>
-      <th>Jeff&nbsp;Wisnia</td>
+      <th>Jeff&nbsp;Wisnia</th>
       <td>Provided technical data sheet for the 6532 RIOT chip</td>
     </tr>
 
     <tr>
       <th>Albert&nbsp;Yarusso<br>
-      (<a href="http://atariage.com">http://atariage.com</a>)</td>
+      (<a href="http://atariage.com">http://atariage.com</a>)</th>
       <td>Provided helpful feedback for the MacOSX port, and generously
         donated an AtariVox device to help with development</td>
     </tr>
 
     <tr>
-      <th>Alex&nbsp;Zaballa</td>
+      <th>Alex&nbsp;Zaballa</th>
       <td>Maintainer of the GP2X version of Stella from releases 2.1 to 2.3.5</td>
     </tr>
 
