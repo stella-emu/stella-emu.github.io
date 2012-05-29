dc18c2e835248bee002fc494dd95b1081d34883f
stephena <stephena@8b62c5a3-ac7e-4cc8-8f21-d9a121418aba>
2012-05-29 19:52:37 +0000

    Some last minute webpage updates.

--- downloads.php
+++ downloads.php
@@ -80,7 +80,7 @@
 	</tr>
 
 	<tr>
-		<td BGCOLOR="#CCCCCC"><a href="http://prdownloads.sourceforge.net/stella/stella-snapshots-20120204.zip?download">stella-snapshots-20120204.zip</a></td>
+		<td BGCOLOR="#CCCCCC"><a href="http://prdownloads.sourceforge.net/stella/stella-snapshots-20120529.zip?download">stella-snapshots-20120529.zip</a></td>
 		<td BGCOLOR="#FFCC99">Snapshot files for use in ROM launcher/ROM Info Viewer</td>
 	</tr>
 
