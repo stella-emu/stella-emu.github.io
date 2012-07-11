c98e7d9e2b5a86f887c70462878d5903a2c1c9c5
stephena <stephena@8b62c5a3-ac7e-4cc8-8f21-d9a121418aba>
2012-07-11 15:00:00 +0000

    Some CSS updates to the "Stella Team" list.

--- htdocs/theteam.php
+++ htdocs/theteam.php
@@ -12,71 +12,71 @@
 			<div class="content">
 
 <!---------------------------------------------------------------->
-<table BORDER=0 CELLPADDING=6 WIDTH="100%" >
+<table class="shaded_content" BORDER=0 CELLPADDING=6 WIDTH="100%" >
 
 <tr>
-	<td><b>Current&nbsp;Members</b></td>
-	<td>&nbsp;</td>
+  <th class="nocolor">Current&nbsp;Members</th>
+  <td class="nocolor">&nbsp;</td>
 </tr>
 
 <tr>
-	<td BGCOLOR="#CCCCCC"><a href="mailto:stephena@users.sourceforge.net">Stephen&nbsp;Anthony</a></td>
-	<td BGCOLOR="#FFCC99">Emulation core/debugger development and enhancements;
-		current maintainer for the Linux, OSX and Windows ports</td>
+  <th><a href="mailto:stephena@users.sourceforge.net">Stephen&nbsp;Anthony</a></th>
+  <td>Emulation core/debugger development and enhancements;
+      current maintainer for the Linux, OSX and Windows ports</td>
 </tr>
 
 <tr>
-	<td BGCOLOR="#CCCCCC"><a href="mailto:bwmott@users.sourceforge.net">Bradford&nbsp;Mott</a></td>
-	<td BGCOLOR="#FFCC99">Original author of Stella</td>
+	<th><a href="mailto:bwmott@users.sourceforge.net">Bradford&nbsp;Mott</a></th>
+	<td>Original author of Stella</td>
 </tr>
 
 <tr>
-	<td BGCOLOR="#CCCCCC"><a href="mailto:estolberg@users.sourceforge.net">Eckhard&nbsp;Stolberg</a></td>
-	<td BGCOLOR="#FFCC99">Emulation core development</td>
+  <th><a href="mailto:estolberg@users.sourceforge.net">Eckhard&nbsp;Stolberg</a></th>
+  <td>Emulation core development</td>
 </tr>
 
 <tr>
-	<td BGCOLOR="#CCCCCC"><a href="mailto:urchlay@users.sourceforge.net">Brian&nbsp;Watson</a></td>
-	<td BGCOLOR="#FFCC99">Emulation core development and debugger support</td>
+  <th><a href="mailto:urchlay@users.sourceforge.net">Brian&nbsp;Watson</a></th>
+  <td>Emulation core development and debugger support</td>
 </tr>
 
 <tr>
-	<td>&nbsp;</td>
-	<td>&nbsp;</td>
+  <td class="nocolor">&nbsp;</td>
+  <td class="nocolor">&nbsp;</td>
 </tr>
 <tr>
-	<td><b>Retired&nbsp;Members</b></td>
-	<td>&nbsp;</td>
-</tr>
-
-<tr>
-	<td BGCOLOR="#CCCCCC">Mark&nbsp;Grebe</td>
-	<td BGCOLOR="#FFCC99">Original author of the Mac OSX port of Stella</td>
+  <th class="nocolor">Retired&nbsp;Members</th>
+  <td class="nocolor">&nbsp;</td>
 </tr>
 
 <tr>
-	<td BGCOLOR="#CCCCCC">Joe&nbsp;D'Andrea</td>
-	<td BGCOLOR="#FFCC99">Maintainer of the Solaris build of Stella</td>
+  <th>Mark&nbsp;Grebe</th>
+  <td>Original author of the Mac OSX port of Stella</td>
 </tr>
 
 <tr>
-	<td BGCOLOR="#CCCCCC">Kostas&nbsp;Nakos</td>
-	<td BGCOLOR="#FFCC99">Author/maintainer of the WinCE port of Stella</td>
+  <th>Joe&nbsp;D'Andrea</th>
+  <td>Maintainer of the Solaris build of Stella</td>
 </tr>
 
 <tr>
-	<td BGCOLOR="#CCCCCC">Doodle</td>
-	<td BGCOLOR="#FFCC99">Maintainer of the OS/2 port of Stella</td>
+  <th>Kostas&nbsp;Nakos</th>
+  <td>Author/maintainer of the WinCE port of Stella</td>
 </tr>
 
 <tr>
-	<td BGCOLOR="#CCCCCC">Darrell&nbsp;Spice&nbsp;Jr.</td>
-	<td BGCOLOR="#FFCC99">Original author of the OS/2 port of Stella</td>
+  <th>Doodle</th>
+  <td>Maintainer of the OS/2 port of Stella</td>
 </tr>
 
 <tr>
-	<td BGCOLOR="#CCCCCC">Alex&nbsp;Zaballa</td>
-	<td BGCOLOR="#FFCC99">Author/maintainer of the GP2X port of Stella</td>
+  <th>Darrell&nbsp;Spice&nbsp;Jr.</th>
+  <td>Original author of the OS/2 port of Stella</td>
+</tr>
+
+<tr>
+  <th>Alex&nbsp;Zaballa</th>
+  <td>Author/maintainer of the GP2X port of Stella</td>
 </tr>
 
 </table>
