e99bdff458cc5fb47749ee46cadf58d6b0304eae
stephena <stephena@8b62c5a3-ac7e-4cc8-8f21-d9a121418aba>
2016-02-06 21:15:28 +0000

    Changed webpage 'Stella News' link to 'Changelog', since that's essentially what it is, and multiple people have asked about the changelog location.

--- footer.php
+++ footer.php
@@ -2,4 +2,4 @@
 <a href="http://www.bradfordmott.com">Bradford W. Mott</a>, 
 <a href="http://minbar.org">Stephen Anthony</a> and 
 <a href="theteam.php">The Stella Team</a>
-<br/>	Site Updated January 25, 2016</p>
+<br/>	Site Updated February 6, 2016</p>
