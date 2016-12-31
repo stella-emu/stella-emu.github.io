8c29e0d20587ab5e427c961f54b0ec156dfc0fbc
Stephen Anthony <sa666666@gmail.com>
2016-12-31 16:31:52 -0330

    Updated online docs to work with Github pages (no php).

--- footer.php
+++
@@ -1,5 +0,0 @@
-<hr><p>Copyright &copy; 1995-<?php echo date("Y"); ?> 
-<a href="http://www.bradfordmott.com">Bradford W. Mott</a>, 
-<a href="http://minbar.org">Stephen Anthony</a> and 
-<a href="theteam.php">The Stella Team</a>
-<br/>	Site Updated November 21, 2016</p>
